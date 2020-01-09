b3e3b64d24b27d5dbf5b314c679e9d1585c341f1
Paul Jimenez <pj@place.org>
2020-01-08 23:19:45 -0500

    add router/ example

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 If you want to use via file:// URLs, you have to (in firefox) go to
 about:config and set privacy.file_unique_origin to False
 
- basic/ - Code from https://johnfraney.ca/posts/2018/08/13/writing-vuejs-app-python/
+ basic/ - code from https://johnfraney.ca/posts/2018/08/13/writing-vuejs-app-python/
 
+ router/ - code from https://router.vuejs.org/guide/ Getting Started
 
---
+++ router/app.py
@@ -0,0 +1,35 @@
+
+
+#// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter
+#// and then call `Vue.use(VueRouter)`.
+
+#// 1. Define route components.
+#// These can be imported from other files
+Foo = dict(template='<div>foo</div>')
+Bar = dict(template='<div>bar</div>')
+
+#// 2. Define some routes
+#// Each route should map to a component. The "component" can
+#// either be an actual component constructor created via
+#// `Vue.extend()`, or just a component options object.
+#// We'll talk about nested routes later.
+routes = [
+  dict(path='/foo', component=Foo),
+  dict(path='/bar', component=Bar)
+]
+
+#// 3. Create the router instance and pass the `routes` option
+#// You can pass in additional options here, but let's
+#// keep it simple for now.
+router = __new__(VueRouter(dict(routes=routes)))
+
+#// 4. Create and mount the root instance.
+#// Make sure to inject the router with the router option to make the
+#// whole app router-aware.
+app = __new__(Vue(dict(
+  router=router
+)))['$mount']('#app')
+
+#// Now the app has started!
+
+
